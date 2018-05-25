17c90cc71c043a6781b7ed2eb576ebed5bebf400
James Traver <james.traver@backstopsolutions.com>
2018-05-25 10:59:19 -0500

    Added JSON API serializer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sample-json-api
+
 Sample JSON API for playing with redux-bees
+
+## Getting Started
+
+1. `npm i` or `npm ci` (npm@5.7.0^) to get your dependencies
+2. `npm run start` will spawn up the JSON API server.
+3. Browse to the address listed in your console.
+4. Test your JSON API!
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 const fortune = require('fortune');
 const http = require('http');
 const fortuneHTTP = require('fortune-http');
+const jsonApiSerializer = require('fortune-json-api');
 
 console.log('📊  Loading Sample JSON API...');
 
 const store = fortune({
-    character: {
-      name: String,
-      companions: [ Array('character'), 'companions'],
-      enemies: [ Array('character'), 'enemies' ],
-      quests: [ Array('quest'), 'questGiver' ]
-    },
-    quest: {
-      message: String,
-      questGiver: [ 'character', 'quests' ]
-    }
-  });
+  character: {
+    name: String,
+    companions: [ Array('character'), 'companions'],
+    enemies: [ Array('character'), 'enemies' ],
+    quests: [ Array('quest'), 'questGiver' ]
+  },
+  quest: {
+    message: String,
+    questGiver: [ 'character', 'quests' ]
+  }
+});
 
-const listener = fortuneHTTP(store);
-const server = http.createServer((req, res) => 
+const listener = fortuneHTTP(store, {
+  serializers: [
+    [jsonApiSerializer]
+  ]
+});
+
+const server = http.createServer((req, res) =>
   listener(req, res).catch(error => { console.log('🔥  Error', error)})
 );
 
